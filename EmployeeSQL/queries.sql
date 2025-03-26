3d93d90282ccf821e842ba8abdf3123a4ab27543
makenna <82896441+makennavick@users.noreply.github.com>
2025-03-26 18:50:25 -0500

    update queries

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -10,10 +10,12 @@ FROM
 INNER JOIN 
 	salaries AS s ON(e.emp_no = s.emp_no)
 
+
 -- List the first name, last name, and hire date for the employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE EXTRACT(YEAR FROM hire_date)=1986
+
 
 -- List the manager of each department along with their department number, department name, employee number, last name, and first name.
 SELECT  
@@ -29,6 +31,7 @@ INNER JOIN
 INNER JOIN 
 	departments AS d ON (d.dept_no=m.dept_no)
 
+
 -- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
 SELECT  
 	d.dept_no, 
@@ -43,6 +46,7 @@ INNER JOIN
 INNER JOIN 
 	departments AS d ON (d.dept_no=de.dept_no)
 
+
 -- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
 SELECT
 	first_name,
@@ -53,9 +57,9 @@ FROM
 WHERE 
 	first_name = 'Hercules'
 	AND last_name LIKE 'B%'
+
 	
 -- List each employee in the Sales department, including their employee number, last name, and first name.
-
 SELECT * FROM DEPARTMENTS -- we can see the sales dept is d007 (identifying it visually prevents the need to do two joins in the next query)
 
 SELECT 
@@ -68,6 +72,7 @@ INNER JOIN
 	dept_emp AS d ON (d.emp_no=e.emp_no)
 WHERE 
 	d.dept_no='d007'
+
 
 -- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
 SELECT
@@ -83,6 +88,7 @@ INNER JOIN
 	departments AS d ON (d.dept_no=de.dept_no)
 WHERE 
 	d.dept_name='Sales' OR d.dept_name='Development'
+
 
 -- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
 SELECT last_name, COUNT(last_name) AS frequency 
--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # sql-challenge
 
+PROMPT
+It’s been two weeks since you were hired as a new data engineer at Pewlett Hackard (a fictional company). Your first major task is to do a research project about people whom the company employed during the 1980s and 1990s. All that remains of the employee database from that period are six CSV files.
+
+## Background
+
 I used [QuickDBD](quickdatabasediagrams.com) to draw up a quick [schema](EmployeeSQL/quickdbd-diagram.png) of the data, exported it to create six tables in PostgreSQL, and imported six corresponding CSVs. Ran into many problems at first until we remembered to remove the serial id column during the imports.
 
 ## Resources
-- S/O to Ethan Donoho for helping me 
-- 
+- S/O to Ethan Donoho 
